9fe777fb73e752009d11392b799b74efb5901e47
leelavenkatsai <primelalith@gmail.com>
2021-07-13 13:08:02 +0530

    playstep done

--- 12-playStep2-Python/playstep2.py
+++ 12-playStep2-Python/playstep2.py
@@ -33,6 +33,53 @@
 # into a sorted hand.
 # Hint: Also, remember to use % to get the one's digit, and use //= to get rid of the one's digit.
 
+
 def playstep2(hand, dice):
 	# your code goes here
-	pass
+	case = casenum(hand)
+	a,b,c = handtodice(hand)
+	orderedHand = dicetoorderedhand(a,b,c)
+	#case1
+	if case == 3:
+		return hand,dice
+	#case2
+	elif case == 2:
+		num1,num2,num3 = handtodice(orderedHand)
+		num4 = dice%10
+		if num1 == num2:
+			newHand = dicetoorderedhand(num1, num2, num4)
+			return newHand, dice//10
+		elif num1 == num3:
+			newHand = dicetoorderedhand(num1,num3,num4)
+			return newHand, dice//10
+		else:
+			newHand = dicetoorderedhand(num3, num2, num4)
+			return newHand, dice//10
+	#case3
+	else:
+		num1, num2, num3 = handtodice(orderedHand)
+		next = dice%100
+		n1, n2, n3 = handtodice(num1*100 + next)
+		newHand = dicetoorderedhand(n1, n2, n3)
+		return newHand, dice//100
+
+def handtodice(hand):
+	# your code goes here
+	hundred = hand//100
+	ten = (hand%100)//10
+	unit = (hand%100)%10
+	return hundred, ten, unit
+def dicetoorderedhand(a, b, c):
+	# your code goes here
+	large = max(a,b,c)
+	small = min(a,b,c)
+	med = a+b+c-large-small
+	return large*(10**2) + med*10 +small
+def casenum(hand):
+	digit1, digit2, digit3 = handtodice(hand)
+	if digit1==digit2==digit3:
+		return 3
+	elif digit1!=digit2!=digit3:
+		return 1
+	else:
+		return 2
