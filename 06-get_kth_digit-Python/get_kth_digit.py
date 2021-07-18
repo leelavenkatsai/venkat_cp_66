109731dc7f7ec2d5ce6949799cce26b737b92903
leelavenkatsai <primelalith@gmail.com>
2021-07-18 20:29:24 +0530

    get done

--- 06-get_kth_digit-Python/get_kth_digit.py
+++ 06-get_kth_digit-Python/get_kth_digit.py
@@ -6,4 +6,14 @@
 
 
 def fun_get_kth_digit(digit, k):
-	return 0
+	if(k>=3):
+		return 0
+	if(digit< 0):
+		digit = digit*-1
+		v = digit//(10**k)
+		n = v%10
+		return n
+	else:
+		v1 = digit//(10**k)
+		n1 = v1%10
+		return n1 
