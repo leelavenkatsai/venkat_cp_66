52e1d7520157960f39a1c835f725226bbac13382
leelavenkatsai <primelalith@gmail.com>
2021-07-21 22:14:57 +0530

    look2 done

--- 06-lookandsay-Python/lookandsay.py
+++ 06-lookandsay-Python/lookandsay.py
@@ -9,33 +9,21 @@
 # lookAndSay([3,3,8,-10,-10,-10]) == [(2,3),(1,8),(3,-10)]
 # lookAndSay([3,3,8,3,3,3,3]) == [(2,3),(1,8),(4,3)]
 
-def readArray():
-    a = []
-    l = int(input())
-    for i in range(l):
-        a.append(int(input()))
-    return a
+def lookandsay(a):
+	# Your code goes here
+	if a == []:
+		return a
+	cnt = 1
+	res = []
+	digit = a[0]
+	for dgt in a[1:]:
+		if dgt == digit:
+			cnt+=1
+		else:
+			res+= [(cnt, digit)]
+			cnt = 1
+			digit = dgt
+	return res+[(cnt, digit)]
 
-def lookAndSay(a):
-    Intfreq=[]
-    initial=1
-    count=0 
-    if(len(a)==0):
-        return []
-    while(True):
-        if(len(a)-1 == count):
-            Intfreq.append((initial,a[count]))
-            break
-        elif(a[count]==a[count+1]):
-            count=count+1
-            initial=initial+1
-        else:
-            Intfreq.append((initial,a[count]))
-            initial=1
-            count=count+1
-    return Intfreq
-    
-
-#lookAndSay(readArray())
 
 
