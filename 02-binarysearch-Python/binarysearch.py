8a956cb1fad3021856680de185201c0a8aa3d3a4
leelavenkatsai <primelalith@gmail.com>
2021-07-26 22:19:17 +0530

    binary search done

--- 02-binarysearch-Python/binarysearch.py
+++ 02-binarysearch-Python/binarysearch.py
@@ -12,4 +12,22 @@ doesn't exist in the list."""
 
 def binary_search(input_array, value):
     # Your code goes here
-    pass
+    # pass
+    final_array = input_array
+    while len(input_array) >= 1:
+    
+        size_of_list= len(input_array)
+    
+        if (size_of_list % 2) != 0:
+            middle_element = input_array[int(size_of_list/2)]
+        else:
+            middle_element = input_array[int((size_of_list/2)-1)]
+    
+        if value == middle_element:
+            return final_array.index(middle_element)
+        elif value < middle_element:
+            input_array = input_array[:(input_array.index(middle_element))]
+        elif value > middle_element:
+            input_array = input_array[(input_array.index(middle_element)+1):]
+
+    return -1
