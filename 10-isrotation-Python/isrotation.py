0e6acaf42a0c55ea0025d24951e6606af6e00c87
leelavenkatsai <primelalith@gmail.com>
2021-07-29 21:43:09 +0530

    isrotation done

--- 10-isrotation-Python/isrotation.py
+++ 10-isrotation-Python/isrotation.py
@@ -5,6 +5,20 @@
 # 3412 is a rotation of 1234. Any number 
 # is a rotation of itself.
 
+
 def isrotation(x, y):
-	# Your code goes here
-	pass
+    # Your code goes here
+    s1 = len(str(x))
+    s2 = len(str(y))
+    tmp = ''
+    # Check if sizes of two strings are same
+    if s1 != s2:
+        return False
+    # Create a temp string with value str1.str1
+    tmp = str(x) + str(x)
+    if (tmp.count(str(y)) > 0):
+        return True
+    elif(str(y)[::-1] == str(x) or str(x)[::-1] == str(y)):
+        return True
+    else:
+        return False
